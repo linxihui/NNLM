ce1c894a3c4c1f23553df7b97126f0aa38af0f0f
linxihui <xihuil.silence@gmail.com>
2015-10-31 14:18:17 -0400

    package info

---
+++ R/pkgname.R
@@ -0,0 +1,13 @@
+#' Fast NNLS and NNMF
+#'
+#' This package is built for fast non-negative least square (NNLS) reguression and non-negative matrix factorization (NNMF).
+#'
+#' @docType package
+#' @name NNLM
+NULL
+
+# @importFrom stats predict
+
+#' @import Rcpp
+#' @useDynLib NNLM
+NULL
