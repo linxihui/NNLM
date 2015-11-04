1379e4ab3676abda3439ef18a316d76e97025247
linxihui <xihuil.silence@gmail.com>
2015-11-04 17:06:27 -0500

    updated

--- src/nnlm.h
+++ src/nnlm.h
@@ -19,11 +19,11 @@
 using namespace arma;
 
 
-mat nnls_solver(const mat & H, mat mu, int max_iter = 1000, double tol = 1e-6, int n_threads = 0);
-mat nnls(const mat & A, const mat & b, int max_iter = 1000, double tol = 1e-6, int n_threads = 1, bool show_progress = false);
-Rcpp::List nmf_nnls(const mat & A, int k, double eta = 0, double beta = 0, int max_iter = 1000, double tol = 1e-6, int n_threads = 1, bool show_progress = false);
+mat nnls_solver(const mat & H, mat mu, int max_iter = 1000, double tol = 1e-4, int n_threads = 0);
+Rcpp::List nnls(const mat & A, const mat & b, int max_iter = 1000, double tol = 1e-4, int n_threads = 1, bool show_progress = false);
+Rcpp::List nmf_nnls(const mat & A, int k, double eta = 0, double beta = 0, int max_iter = 1000, double tol = 1e-4, int n_threads = 1, bool show_progress = false);
 
 
-Rcpp::List nmf_brunet(const mat & A, int k, int max_iter = 1000 , double tol = 1e-6, int n_threads = 1, bool show_progress = false);
-mat get_H_brunet(const mat & A, const mat & W, int max_iter = 10000, double tol = 1e-6, int n_threads = 1, bool show_progress = false);
+Rcpp::List nmf_brunet(const mat & A, int k, int max_iter = 1000 , double tol = 1e-4, int n_threads = 1, bool show_progress = false);
+mat get_H_brunet(const mat & A, const mat & W, int max_iter = 10000, double tol = 1e-4, int n_threads = 1, bool show_progress = false);
 // Rcpp::List nmf_ols(mat A, int k, double eta, double beta, int max_iter, double tol); 
