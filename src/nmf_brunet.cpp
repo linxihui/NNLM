a8a6fc1f1cde8de9f7e19e5bad0c5f96c4ca05f9
linxihui <xihuil.silence@gmail.com>
2015-11-04 17:00:13 -0500

    changed stop criterion to rel.tol; moved get_H_brunet to indepedent file

--- src/nmf_brunet.cpp
+++ src/nmf_brunet.cpp
@@ -51,19 +51,16 @@ Rcpp::List nmf_brunet(const mat & A, int k, int max_iter , double tol, int n_thr
 			Abar += W.col(a) * (H.row(a) - ha);
 		}
 		err.at(i) =  std::sqrt(mean(mean(square(A - Abar))));
-		trgt_err.at(i) = mean(mean(A % (arma::trunc_log(A) - arma::trunc_log(Abar)) - A - Abar));
-		if (i > 0 && std::abs(trgt_err.at(i) - trgt_err.at(i-1)) < tol)
-		{
-			err.resize(i);
-			trgt_err.resize(i);
+		trgt_err.at(i) = accu(A % (arma::trunc_log(A) - arma::trunc_log(Abar)) - A - Abar);
+		if (i > 0 && std::abs(trgt_err.at(i) - trgt_err.at(i-1))/(std::abs(trgt_err.at(i-1)) + 1e-6) < tol)
 			break;
-		}
 	}
 
 	if (max_iter <= i)
-	{
 		Rcpp::warning("Target tolerence not reached. Try a larger max.iter.");
-	}
+
+	err.resize(i < max_iter ? i+1 : max_iter);
+	trgt_err.resize(err.n_elem);
 
 	return Rcpp::List::create(
 		Rcpp::Named("W") = W,
@@ -71,54 +68,4 @@ Rcpp::List nmf_brunet(const mat & A, int k, int max_iter , double tol, int n_thr
 		Rcpp::Named("error") = err,
 		Rcpp::Named("target_error") = trgt_err
 		);
-}
-
-
-//[[Rcpp::export]]
-mat get_H_brunet(const mat & A, const mat & W,  int max_iter, double tol, int n_threads, bool show_progress)
-{
-	/*
-	 * Description:
-	 * 	Get coefficient matrix H given A and W, where A ~ W*H
-	 */
-
-	int k = W.n_cols;
-	mat H = randu(k, A.n_cols);
-	mat Abar = W*H;
-	rowvec w = sum(W), ha;
-	colvec h;
-	vec err(max_iter);
-	err.fill(-1);
-
-	// check progression
-	Progress prgrss(max_iter, show_progress);
-
-	int i = 0;
-	for(; i < max_iter; i++)
-	{
-		Rcpp::checkUserInterrupt();
-
-		prgrss.increment();
-
-		h = sum(H, 1);
-		for (int a = 0; a < k; a++)
-		{
-			ha = H.row(a);
-			H.row(a) %= W.col(a).t() * (A / Abar) / w.at(a);
-			Abar += W.col(a) * (H.row(a) - ha);
-		}
-		err.at(i) = mean(mean(A % (arma::trunc_log(A) - arma::trunc_log(Abar)) - A - Abar));
-		if (i > 0 && std::abs(err.at(i) - err.at(i-1)) < tol)
-		{
-			err.resize(i);
-			break;
-		}
-	}
-
-	if (max_iter <= i)
-	{
-		Rcpp::warning("Target tolerence not reached. Try a larger max.iter.");
-	}
-
-	return H;
 }
