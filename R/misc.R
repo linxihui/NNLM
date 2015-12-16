1bf7f9d671215e63ceb740122f0bb6680ba34c71
linxihui <xihuil.silence@gmail.com>
2015-12-15 23:48:28 -0500

    auxiliary functions

---
+++ R/misc.R
@@ -0,0 +1,175 @@
+# # check if input is a matrix, non-negative and no-missing
+# #
+# # @param A Input matrix to be check
+# # @return A properly modified matrix
+# check.input.matrix <- function(A, check.missing = FALSE) {
+# 	input.name <- as.character(substitute(A));
+# 	if (!is.matrix(A)) A <- as.matrix(A);
+# 	if (!is.numeric(A) && !is.logical(A)) stop(sprintf("Matrix %s must be numeric", input.name));
+# 	if (!is.double(A)) storage.mode(A) <- 'double';
+# 	if (any(A[!is.na(A)] < 0)) stop(sprintf("Matrix %s must be non-negative.", input.name));
+# 	if (check.missing && any(is.na(A))) stop(sprintf("Matrix %s contains missing values.", input.name));
+# 	return(A);
+# 	}
+# 
+# # check if input is a mask matrix, non-negative and no-missing
+# #
+# # @param mask Input matrix to be check
+# # @return A properly modified matrix
+# check.input.mask <- function(mask) {
+# 	input.name <- as.character(substitute(mask));
+# 	if (!is.matrix(mask)) mask <- as.matrix(mask);
+# 	if (!is.logical(mask)) storage.mode(mask) <- 'logical';
+# 	if (any(is.na(mask))) stop(sprintf("Matrix %s contains missing values.", input.name));
+# 	return(mask);
+# 	}
+# 
+# # check if an initial matrix and return a proper one
+# #
+# # @param init Initial matrix
+# # @param dim Expected dimension of init
+# # @param vars A vector of variable names for print out
+# # @param mode Expected storange mode of init
+# # @return A properly modified matrix
+# check.input.init <- function(init, dim, vars = c('x', 'y'), mode = 'double') {
+# 	input.name <- as.character(substitute(init));
+# 	if (is.null(init))
+# 		return(matrix(0.0, nrow = dim[1], ncol = dim[2]));
+# 	if (!is.matrix(init)) init <- as.matrix(init);
+# 	if (any(dim(init) != dim))
+# 		stop(paste0(
+# 				"Dimension of ", input.name, " (if specified) must be compatible with ", 
+# 				paste(vars, collapse = ', '), "."
+# 				));
+# 	if (storage.mode(init) != mode) storage.mode(init) <- mode;
+# 	if (any(is.na(init))) stop(sprintf("Matrix %s contains missing values.", input.name));
+# 	return(init);
+# 	}
+
+#' Compute mean square error(MSE) and mean kL divergence (MKL)
+#' 
+#' @param obs          observed value
+#' @param pred         prediction/estimate
+#' @param na.rm        if to remove NAs
+#' @param show.warning if to show warning if any
+#' @return A vector of c(MSE, MKL)
+#' @export
+mse.mkl <- function(obs, pred, na.rm = TRUE, show.warning = TRUE) {
+	mkl <- ifelse(
+		!show.warning && (any(obs < 0) || any(pred < 0)), NaN, 
+		mean((obs + 1e-16)*log((obs+1e-16) / (pred+1e-16)) - obs + pred, na.rm = na.rm)
+		);
+	mse <- mean((obs - pred)^2, na.rm = na.rm)
+	return(c(MSE = mse, MKL = mkl));
+	}
+
+# Get method code passed to C++ functions
+# 	1 = "scd" + "mse"
+# 	2 = "lee" + "mse"
+# 	3 = "scd" + "mkl"
+# 	4 = "lee" + "mkl"
+#
+# @param method Either sequential coodinate-wise descent (SCD) or Lee's multiplicative algorithm
+# @param loss Loss function, either mean square error (MSE) or mean KL-divergence (MKL)
+# @return method code from 1L - 4L
+#
+get.method.code <- function(method = c('scd', 'lee'), loss = c('mse', 'mkl')) {
+	method <- match.arg(method);
+	loss <- match.arg(loss);
+	code <- 1L;
+	if ('mkl' == loss) code <- code + 2L;
+	if ('lee' == method) code <- code + 1L;
+	return(code);
+	}
+
+
+check.matrix <- function(A, dm = NULL, mode = 'numeric', check.na = FALSE, input.name = '', check.negative = FALSE) {
+	if (is.null(A)) return(invisible(NULL));
+	if (!is.null(dm) && any(dim(A) != dm, na.rm = TRUE))
+		stop(sprintf("Dimension of matrix %s is expected to be (%d, %d), but got (%d, %d)", input.name, nrow(A), ncol(A), dm[1], dm[2]));
+	if (mode(A) != mode) stop(sprintf("Matrix %s must be %s.", input.name, mode));
+	if (check.negative && any(A[!is.na(A)] < 0)) stop(sprintf("Matrix %s must be non-negative.", input.name));
+	if (check.na && any(is.na(A))) stop(sprintf("Matrix %s contains missing values.", input.name));
+	}
+
+
+reformat.input <- function(init, mask, n, m, k) {
+	if (is.null(mask)) mask <- list();
+	if (is.null(init)) init <- list();
+	stopifnot(is.list(mask));
+	stopifnot(is.list(init));
+
+	known.W <- !is.null(init[['W0']]);
+	known.H <- !is.null(init[['H0']]);
+	kW0 <- kH0 <- 0;
+
+	is.empty <- function(x) 0 == length(x);
+
+	if (known.W) {
+		if(!is.matrix(init[['W0']]))
+			init[['W0']] <- as.matrix(init[['W0']]);
+		kW0 <- ncol(init[['W0']]);
+		mask[['W0']] <- matrix(TRUE, n, kW0);
+		}
+	else {
+		mask[['W0']] <- NULL;
+		mask[['H1']] <- NULL;
+		init[['H1']] <- NULL;
+		}
+
+	if (known.H) {
+		if(!is.matrix(init$H0))
+			init[['H0']] <- as.matrix(init[['H0']]);
+		kH0 <- nrow(init[['H0']]);
+		mask[['H0']] <- matrix(TRUE, kH0, m);
+		}
+	else {
+		mask[['H0']] <- NULL;
+		mask[['W1']] <- NULL;
+		init[['W1']] <- NULL;
+		}
+
+	K <- k + kW0 + kH0;
+
+	is.W.masked <- !all(sapply(mask[c('W', 'W0', 'W1')], is.empty));
+	is.H.masked <- !all(sapply(mask[c('H', 'H0', 'H1')], is.empty));
+	nWm <- ifelse(is.W.masked, n, 0);
+	mHm <- ifelse(is.H.masked, m, 0);
+	dim.mask <- list('W' = c(nWm, k), 'W0' = c(nWm, kW0), 'W1' = c(nWm, kH0), 
+		'H' = c(k, mHm), 'H1' = c(kW0, mHm), 'H0' = c(kH0, mHm));
+
+	for (mat in c('W', 'W0', 'W1', 'H' ,'H0', 'H1')) {
+		check.matrix(mask[[mat]], dim.mask[[mat]], 'logical', TRUE, paste0('mask$', mat));
+		if (is.empty(mask[[mat]]))
+			mask[[mat]] <- matrix(FALSE, dim.mask[[mat]][[1]], dim.mask[[mat]][[2]]);
+		}
+
+	is.W.not.empty <- !all(sapply(init[c('W', 'W0', 'W1')], is.empty));
+	is.H.not.empty <- !all(sapply(init[c('H', 'H0', 'H1')], is.empty));
+	nWi <- ifelse(is.W.not.empty, n, nWm);
+	mHi <- ifelse(is.H.not.empty, m, mHm);
+	dim.init <- list(
+		'W' = c(nWi, k), 'W0' = c(nWi, kW0), 'W1' = c(nWi, kH0), 
+		'H' = c(k, mHi), 'H1' = c(kW0, mHi), 'H0' = c(kH0, mHi)
+		);
+	for (mat in c('W', 'W0', 'W1', 'H' ,'H0', 'H1')) {
+		check.matrix(init[[mat]], dim.init[[mat]], 'numeric', TRUE, paste0('init$', mat));
+		if (is.empty(init[[mat]])) {
+			init[[mat]] <- matrix(runif(prod(dim.init[[mat]])), dim.init[[mat]][[1]], dim.init[[mat]][[2]]);
+			init[[mat]][mask[[mat]]] <- 0;
+			}
+		if (!is.double(init[[mat]]))
+			storage.mode(init[[mat]]) <- 'double';
+		}
+
+	return(
+		list(
+			Wm = do.call(cbind, mask[c('W', 'W0', 'W1')]),
+			Hm = do.call(rbind, mask[c('H', 'H1', 'H0')]),
+			Wi = do.call(cbind, init[c('W', 'W0', 'W1')]),
+			Hi = do.call(rbind, init[c('H', 'H1', 'H0')]),
+			kW0 = kW0,
+			kH0 = kH0,
+			K = K
+		));
+	}
