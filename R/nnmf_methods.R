ebad01b38cf7731d05285aa47acb0484c024e307
linxihui <xihuil.silence@gmail.com>
2015-12-16 23:26:00 -0500

    modified predict.nnmf, better print function

--- R/nnmf_methods.R
+++ R/nnmf_methods.R
@@ -1,20 +1,14 @@
 #' Methods for nnmf object returned by \code{nnmf}
 #'
 #' @param object        An NNMF object returned by \code{\link{nnmf}}
-#' @param newdata       A new matrix of x
-#' @param which.matrix  Either 'W' or 'H'
-#' @param method        Either 'nnls' or 'brunet'. Default to \code{object$method}
-#' @param max.iter      Maximum number of iterations
-#' @param rel.tol       Stop criterion, relative difference of target_error between two successive iterations
-#' @param n.threads     An integer number of threads/CPUs to use. Default to 1(no parallel). Use 0 for all cores
-#' @param show.progress TRUE/FALSE indicating if to show a progress bar
-#' @param show.warning  If to show warnings when targeted \code{rel.tol} is not reached
-#' @param ...           Further arguments passed to 'print', 'plot' or 'heatmap'
-#' @return \itemize{
-#' 	\item{predict: }{\code{W} or \code{H} for newdata given pre-computed H or W if \code{W0} and \code{H0} are NULL, else a list of \code{W, W1} or \code{H, H1}}
-#' 	\item{print: }{print running time and accuracy}
-#' 	\item{plot: }{line plot if which = 'error' or 'target.error' and heatmap if which = 'H' or 'W'}
-#' 	}
+#' @param newdata       A new matrix of x. No requred when \code{which == 'A'}
+#' @param which         Either 'A' (default), 'W' or 'H'
+#' @param method        Either 'scd' or 'lee'. Default to \code{object$options$method}
+#' @param loss          Either 'mse' or 'mkl'. Default to \code{object$options$loss}
+#' @param x             An NNMF object returned by \code{\link{nnmf}}
+#' @param ...           Further arguments passed to 'nnlm' or 'print'
+#' @return 'A' or a class of 'nnlm' for 'predict.nnmf' and no return for 'print'
+#'
 #' @examples
 #'
 #' x <- matrix(runif(50*20), 50, 20)
@@ -22,122 +16,67 @@
 #' r
 #' newx <- matrix(runif(50*30), 50, 30)
 #' pred <- predict(r, newx, 'H')
-#' 
-#' # check convergence
-#' plot(r)
 #'
-#' # show W matrix
-#' plot(r, 'W')
-#'
-#' @seealso \code{\link{nnmf}}
+#' @seealso \code{\link{nnmf}}, \code{\link{nnlm}}
 #' @export
 predict.nnmf <- function(
-	object, newdata, which.matrix = c('H', 'W'), method = object$method, 
-	max.iter = 100L, rel.tol = object$rel.tol, n.threads = 1L, 
-	show.progress = TRUE, show.warning = TRUE,
+	object, newdata, which = c('A', 'W', 'H'),
+	method = object$options$method,
+	loss = object$options$loss,
 	...) {
-	which.matrix <- match.arg(which.matrix);
-	check.input.matrix(newdata);
-	if (!is.matrix(newdata)) newdata <- as.matrix(newdata);
-	if (!is.double(newdata)) storage.mode(newdata) <- 'double';
-	if (!all(newdata >= 0)) stop("newdata must be non-negative");
-	rel.tol <- as.double(rel.tol);
-	max.iter <- as.integer(max.iter);
 
-	method <- match.arg(method, c('nnls', 'brunet'));
+	which <- match.arg(which);
+	if (which != 'A') {
+		if('W' == which)
+			check.matrix(newdata, dm = c(NA, ncol(object$H)));
+		if('H' == which)
+			check.matrix(newdata, dm = c(nrow(object$W),NA ));
+		if (!is.double(newdata))
+			storage.mode(newdata) <- 'double';
+		}
 
-	out <- switch(method,
-		'nnls' = {
-			out <- switch(which.matrix,
-				'H' = {
-					if(!is.null(object$H0)) newdata <- newdata - object$W1 %*% object$H0;
-					W <- object$W;
-					if(!is.null(object$W0)) W <- cbind(W, object$W0);
-					nnls(W, newdata, check.x = TRUE, max.iter = max.iter, rel.tol = rel.tol, 
-						n.threads = n.threads, show.progress = show.progress
-						)$coefficients
-					},
-				'W' = {
-					if(!is.null(object$W0)) newdata <- newdata - object$W0 %*% object$H1;
-					H <- t(object$H);
-					if(!is.null(object$H0)) H <- cbind(H, t(object$H0));
-					t(nnls(H, t(newdata), 
-						check.x = TRUE, max.iter = max.iter, rel.tol = rel.tol, 
-						n.threads = n.threads, show.progress = show.progress
-						)$coefficients)
-					}
-				)
-			},
-		'brunet' = {
-			switch(which.matrix,
-				'H' = .Call('NNLM_get_H_brunet', 
-					newdata, object$W, max.iter, rel.tol, n.threads, show.progress, show.warning,
-					PACKAGE = 'NNLM'
-					),
-				'W' = t(.Call( 'NNLM_get_H_brunet', 
-					t(newdata), t(object$H), max.iter, rel.tol, n.threads, show.progress, show.warning,
-					PACKAGE = 'NNLM')
-					)
-				)
-			}
+	out <- switch(which,
+		'A' = object$W %*% object$H,
+		'W' = nnlm(t(object$H), t(newdata), method = method, loss = loss, ...),
+		'H' = nnlm(object$W, newdata, method = method, loss = loss, ...)
 		);
 
-	if ('H' == which.matrix) colnames(out) <- colnames(newdata);
-	if ('W' == which.matrix) rownames(out) <- rownames(newdata);
+	if ('W' == which)
+		out$coefficients <- t(out$coefficients);
 
-	if ('H' == which.matrix && !is.null(object$W0))
-		out <- list(
-			H = out[seq_len(ncol(object$H)), , drop = FALSE],
-			H1 = out[-seq_len(ncol(object$H)), , drop = FALSE]
-			);
-
-	if ('W' == which.matrix && !is.null(object$H0))
-		out <- list(
-			W = out[, seq_len(ncol(object$W)), drop = FALSE],
-			W1 = out[, -seq_len(ncol(object$W)), drop = FALSE]
-			);
-
- 	return(out);
+	return(out);
 	}
 
 
 #' @rdname predict.nnmf
 #' @export
 print.nnmf <- function(x, ...) {
-	print(x$system.time);
-	nstep <- length(x$error);
-	cat('RMSE: ', x$error[nstep], '\n', sep='');
-	switch(x$target.measure,
-		'Kullback-Leibler Divergence' = cat('KL divergence: ', x$target.error[nstep], '\n', sep=''),
-		'Penalized Root Mean Square Error' = cat('Penalized RMSE: ', x$target.error[nstep], '\n', sep='')
-		);
+	if (x$n.iteration < 2) {
+		rel.tol <- NA_real_;
+	} else {
+		err <- tail(x$target.loss, 2);
+		rel.tol <- diff(err)/mean(err); 
+		}
+	cat("Non-negative matrix factorization:\n")
+	if (x$options$method == 'scd') {
+		cat("   Algorithm: Sequential coordinate-wise descent\n");
+	} else {
+		cat("   Algorithm: Lee's multiplicative algorithm\n");
+		}
+	if (x$options$loss == 'mse') {
+		cat("        Loss: Mean squared error\n");
+	} else {
+		cat("        Loss: Mean Kullback-Leibler divergence\n");
+		}
+		cat("         MSE: ", tail(x$mse, 1), '\n', sep = '');
+		cat("         MKL: ", tail(x$mkl, 1), '\n', sep = '');
+		cat("      Target: ", tail(x$target.loss, 1), '\n', sep = '');
+		cat("   Rel. tol.: ", sprintf("%.3g", abs(rel.tol)), '\n', sep = '');
+		cat("Total epochs: ", as.integer(sum(x$average.epochs)), '\n', sep = '');
+		cat("# Interation: ", x$n.iteration, '\n', sep = '');
+		cat("Running time:\n");
+	print(x$run.time);
+	invisible(NULL);
 	}
 
-#' @rdname predict.nnmf
-#' @param x     An NNMF object returned by \code{\link{nnmf}}
-#' @param which One of 'error', 'target', 'W', 'H', 'W1', 'H1'. The first two give line plots while the latter two give heatmaps
-#' @export
-plot.nnmf <- function(x, which = c('error', 'target.error', 'W', 'H', 'W1', 'H1'),  ...) {
-	which <- match.arg(which);
-	dots <- list(...);
-
-	default.setting <- list(
-		x[[which]],
-		type = 'l', 
-		lwd = 2,
-		xlab = 'Iteration',
-		ylab = which
-		);
-	dots <- modifyList(default.setting, dots);
-
-	switch(which,
-		'error' = ,
-		'target.error' = do.call(plot, dots),
-		'W' = , 'H' = ,
-		'W1' = , 'H1' = {
-			if(is.null(x[[which]]))
-				stop(sprintf("%s not found.", which));
-			heatmap(x[[which]], ...)
-			}
-		)
-	}
+# tunning k
